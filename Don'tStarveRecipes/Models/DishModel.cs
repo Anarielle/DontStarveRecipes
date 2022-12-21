eae6a7db2f9f1b979b12478894a835fec35855b0
Anarielle A <anarielle.a@gmail.com>
2022-12-22 00:14:24 +0300

    added ingredients

---
+++ Don'tStarveRecipes/Models/DishModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Don_tStarveRecipes.Models
+{
+    public class DishModel
+    {
+        public string DishName { get; set; }
+        public string ImagePath { get; set; }
+        public bool Drumstick { get; set; }
+        public bool Meat { get; set; }
+        public bool Morsel { get; set; }
+        public bool FrogLegs { get; set; }
+    }
+}
---
+++ Don'tStarveRecipes/Models/IngredientsModel.cs
@@ -0,0 +1,55 @@
+﻿namespace Don_tStarveRecipes.Models
+{
+    public class IngredientsModel
+    {
+        public enum Meats
+        {
+            Drumstick,
+            Eel,
+            Fish,
+            FrogLegs,
+            LeafyMeat,
+            Meat,
+            MonsterMeat,
+            Morsel
+        }
+
+        public enum Fruits
+        {
+            Berries,
+            CaveBanana,
+            DragonFruit,
+            Durian,
+            Pomegranate,
+            Watermelon
+        }
+
+        public enum Vegetables
+        {
+            BlueCap,
+            CactusFlesh,
+            CactusFlower,
+            Carrot,
+            Corn,
+            Eggplant,
+            GreenCap,
+            Lichen,
+            Mandrake,
+            Pumpkin,
+            RedCap
+        }
+
+        public enum Other
+        {
+            Butter,
+            ButterflyWings,
+            Egg,
+            ElectricMilk,
+            Honey,
+            Honeycomb,
+            Ice,
+            TallbirdEgg,
+            Twigs
+        }
+    }
+}
